af63e4d690ef454b0e73a1a5d2ae0876537a367d
Stéphane Brunner <courriel@stephane-brunner.ch>
2011-12-30 11:24:57 +0100

    add ondo and redo on geometre drawing

--- osmeditor/app/lib/App/CreatePolygon.js
+++ osmeditor/app/lib/App/CreatePolygon.js
@@ -19,6 +19,35 @@ App.CreatePolygon = Ext.extend(gxp.plugins.Tool, {
         this.control = new OpenLayers.Control.DrawFeature(
                 this.target.mapPanel.map.getLayersByName("OSM")[0],
                 OpenLayers.Handler.Polygon);
+        var control = this.control;
+
+        OpenLayers.Event.observe(document, "keydown", function(evt) {
+            if (control.active) {
+                var handled = false;
+                switch (evt.keyCode) {
+                    case 90: // z
+                        if (evt.metaKey || evt.ctrlKey) {
+                            control.undo();
+                            handled = true;
+                        }
+                        break;
+                    case 89: // y
+                        if (evt.metaKey || evt.ctrlKey) {
+                            control.redo();
+                            handled = true;
+                        }
+                        break;
+                    case 27: // esc
+                        control.cancel();
+                        handled = true;
+                        break;
+                }
+                if (handled) {
+                    OpenLayers.Event.stop(evt);
+                }
+            }
+        });
+
         this.map = this.target.mapPanel.map;
         var actions = [new GeoExt.Action(this)];
         return App.CreatePolygon.superclass.addActions.apply(this, [actions]);
