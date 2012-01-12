816df05315857b31193a117784b2b7758296901f
Stéphane Brunner <courriel@stephane-brunner.ch>
2012-01-12 21:03:27 +0100

    enable client zoom

--- osmeditor/app/lib/App/Editor.js
+++ osmeditor/app/lib/App/Editor.js
@@ -62,6 +62,19 @@ Ext.onReady(function() {
     };
     permalinkProvider.on({statechange: onStatechange});
 
+    var osmResolutions = [156543.03390625, 78271.516953125, 39135.7584765625,
+            19567.87923828125, 9783.939619140625,
+            4891.9698095703125, 2445.9849047851562,
+            1222.9924523925781, 611.4962261962891,
+            305.74811309814453, 152.87405654907226,
+            76.43702827453613, 38.218514137268066,
+            19.109257068634033, 9.554628534317017,
+            4.777314267158508, 2.388657133579254,
+            1.194328566789627, 0.5971642833948135]
+    var bingResolutions = osmResolutions.concat([0.29858214169740677]);
+    var mapResolutions = bingResolutions.concat([
+            0.14929107084870338, 0.07464553542435169]);
+
     app = new gxp.Viewer({
         portalConfig: {
             layout: "border",
@@ -167,7 +180,9 @@ Ext.onReady(function() {
                     sphericalMercator: true,
                     wrapDateLine: true,
                     isBaseLayer: true,
-                    numZoomLevels: 22
+                    transitionEffect: 'resize',
+                    resolutions: mapResolutions,
+                    serverResolutions: mapResolutions
                 }]
             }, {
                 source: "ol",
@@ -181,7 +196,9 @@ Ext.onReady(function() {
                     sphericalMercator: true,
                     wrapDateLine: true,
                     isBaseLayer: false,
-                    numZoomLevels: 19,
+                    transitionEffect: 'resize',
+                    resolutions: mapResolutions,
+                    serverResolutions: osmResolutions,
                     attribution: "Data CC-By-SA by <a href='http://openstreetmap.org/' target='_blank'>OpenStreetMap</a>"
                 }]
             }, {
@@ -190,7 +207,9 @@ Ext.onReady(function() {
                 args: [{
                     key: 'At1BfDJ7AORxoIDB3XWbl-LrBoS2LiofC9yirzytRrmFJK0sUD6jwCODSXuDas70',
                     type: 'Aerial',
-                    ref: 'ortho'
+                    ref: 'ortho',
+                    resolutions: mapResolutions,
+                    serverResolutions: bingResolutions
                 }]
             }],
             controls: [
